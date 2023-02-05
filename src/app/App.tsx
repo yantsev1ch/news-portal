de479ab7076b7b75712c003b4aa008ff1bd200cc
yantsev1ch <yantsevich92@gmail.com>
2023-02-05 23:50:08 +0300

    added AppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {MainPage} from "pages/MainPage";
-import {AboutPage} from 'pages/AboutPage';
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -13,12 +12,7 @@ const App = () => {
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <button onClick={toggleTheme}>Toggle</button>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
