0165b9a124167f15cbe8decc1b35ead6959ed75f
yantsev1ch <yantsevich92@gmail.com>
2023-02-23 13:52:07 +0300

    fixed tests

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -4,9 +4,16 @@ import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
 import { MemoryRouter } from 'react-router-dom';
 
-export function componentRender(component: ReactNode) {
+export interface ComponentRenderOptions {
+    route?: string;
+}
+
+export function componentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+  const {
+    route = '/',
+  } = options;
   return render(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={[route]}>
       <I18nextProvider i18n={i18nForTests}>
         {component}
       </I18nextProvider>
