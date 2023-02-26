683963065d0b0de58d569167e03a3cc4b283fd17
yantsev1ch <yantsevich92@gmail.com>
2023-02-26 20:22:19 +0300

    added redux store, added first entity

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,22 @@
+import { CounterSchema } from 'entities/Counter';
+import { counterActions, counterReducer } from './counterSlice';
+
+describe('counterSlice.test', () => {
+  test('increment', () => {
+    const state: CounterSchema = {
+      value: 10,
+    };
+    expect(counterReducer(state, counterActions.increment())).toEqual({ value: 11 });
+  });
+
+  test('decrement', () => {
+    const state: CounterSchema = {
+      value: 10,
+    };
+    expect(counterReducer(state, counterActions.decrement())).toEqual({ value: 9 });
+  });
+
+  test('should work with empty state', () => {
+    expect(counterReducer(undefined, counterActions.increment())).toEqual({ value: 1 });
+  });
+});
