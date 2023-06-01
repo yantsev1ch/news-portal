7ee807bef96b1ba08c9aab4bd2e2eafbf357565b
yantsev1ch <yantsevich92@gmail.com>
2023-06-01 08:59:25 +0300

    added circular dependency plugin, fixed

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -42,6 +43,12 @@ export function buildPlugins({
     plugins.push(
       new BundleAnalyzerPlugin({
         openAnalyzer: false,
+      }),
+    );
+    plugins.push(
+      new CircularDependencyPlugin({
+        exclude: /node_modules/,
+        failOnError: true,
       }),
     );
   }
--- src/pages/ArticlesPage/index.ts
+++ src/pages/ArticlesPage/index.ts
@@ -1,4 +1,3 @@
 export { ArticlesPageAsync as ArticlesPage } from './ui/ArticlesPage/ArticlesPage.async';
 
-export { articlesPageReducer } from './model/slice/articlesPageSlice';
 export type { ArticlesPageSchema } from './model/types/articlesPageSchema';
