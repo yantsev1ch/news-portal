7d8b44ed1dd0d447e5b0372437ba8296fb4c9e23
Andrey Yantsevich <yantsevich92@gmail.com>
2023-10-25 15:55:42 +0300

    fix reducers list

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -3,11 +3,12 @@ import { useDispatch, useStore } from 'react-redux';
 import { Reducer } from '@reduxjs/toolkit';
 import {
   ReduxStoreWithManager,
+  StateSchema,
   StateSchemaKey,
 } from '@/app/providers/StoreProvider';
 
 export type ReducersList = {
-  [name in StateSchemaKey]?: Reducer;
+  [name in StateSchemaKey]?: Reducer<NonNullable<StateSchema[name]>>;
 };
 
 interface DynamicModuleLoaderProps {
