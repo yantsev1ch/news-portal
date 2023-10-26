1fd97f118f92663a9c5948b5f13b9001df109ec8
Andrey Yantsevich <yantsevich92@gmail.com>
2023-10-26 16:21:52 +0300

    babel loader cache, postinstall hook

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,7 +6,9 @@ import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
 import { buildDevServer } from './buildDevServer';
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+export function buildWebpackConfig(
+  options: BuildOptions,
+): webpack.Configuration {
   const { paths, mode, isDev } = options;
 
   return {
@@ -23,7 +25,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
       rules: buildLoaders(options),
     },
     resolve: buildResolvers(options),
-    devtool: isDev ? 'inline-source-map' : undefined,
+    devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -6,21 +6,16 @@ interface BuildBabelLoaderProps extends BuildOptions {
 }
 
 export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
+  const isProd = !isDev;
   return {
     test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
     exclude: /node_modules/,
     use: {
+      // cacheDirectory: true,
       loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env'],
         plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['ru', 'en'],
-              keyAsDefaultValue: true,
-            },
-          ],
           [
             '@babel/plugin-transform-typescript',
             {
@@ -29,7 +24,7 @@ export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
           ],
           '@babel/plugin-transform-runtime',
           isTsx
-            && !isDev && [
+            && isProd && [
             babelRemovePropsPlugin,
             {
               props: ['data-testid'],
