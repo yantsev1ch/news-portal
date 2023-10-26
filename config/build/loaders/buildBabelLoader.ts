60708cf68da13daabdb3cbb76b1721cf6929b947
Andrey Yantsevich <yantsevich92@gmail.com>
2023-10-26 16:25:30 +0300

    babel loader cache, postinstall hook

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -12,7 +12,7 @@ export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
     exclude: /node_modules/,
     use: {
       // cacheDirectory: true,
-      loader: 'babel-loader',
+      loader: 'babel-loader?optional=runtime&cacheDirectory',
       options: {
         presets: ['@babel/preset-env'],
         plugins: [
