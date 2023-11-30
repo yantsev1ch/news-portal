3395da4b6592239689380259f1e53cd84dc9664a
Andrey Yantsevich <yantsevich92@gmail.com>
2023-11-30 15:39:49 +0300

    add report to ci

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,9 +1,8 @@
 import { addDecorator } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { Theme } from '../../src/shared/const/theme';
+import { Theme } from '../../src';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
